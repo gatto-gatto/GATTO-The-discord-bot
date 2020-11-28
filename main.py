d7f078265df6c87ff269120ea077e9e5eca30588
gatto-gatto <mehtadinku@gmail.com>
2020-11-28 17:28:42 +0530

    puspus delted

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ async def type(ctx):
 		msg=await client.wait_for('message')
 		if(msg.content=="gatto"):
 			await ctx.channel.purge(limit=cnt)
-			break
+			break	
 		if(msg.content!=local):
 			await ctx.send("wrong")
 			cnt+=1
 
-
+	
 @client.command()
 async def fastfinger(ctx):
 	import random
@@ -77,93 +77,6 @@ async def sins(ctx,amount=50):
 		await ctx.channel.purge(limit=1000)
 		sleep(60*5)
 
-@client.command()
-async def puspus(ctx):
-	UTC = pytz.utc 
-	timeZ_Kl = pytz.timezone('Asia/Kolkata') 
-	dt_Kl = datetime.now(timeZ_Kl) 
-	# finding the time aoocind to 24 hrs clock........ local time
-	time1= int(dt_Kl.strftime('%Y-%m-%d %H:%M:%S %Z %z').split()[1].split(':')[0])
-	#finding the day hdsiojd 
-	day=time.ctime().split()[0]
-	await ctx.send(f"{time1} , {day}")
-	if(day=='Mon'):
-		if(time1==1+12):
-			await ctx.send("AP by varun")			
-			await ctx.send("https://meet.google.com/lookup/htvdf4mslf")			
-		elif(time1==2+12):
-			await ctx.send("COA")			
-			await ctx.send("https://meet.google.com/bnd-gsza-oyf?pli=1&authuser=1")
-		elif(time1==3+12):
-			await ctx.send("IDBMS")
-			await ctx.send("https://meet.google.com/lookup/evx4lzq2xa?authuser=1&hs=179")
-		elif(time1==4+12):		
-			await ctx.send("DCS")
-			await ctx.send("https://meet.google.com/lookup/gcfpz2o5ch?authuser=1&hs=179")
-		elif(time1==5+12):		
-			await ctx.send("M3")
-			await ctx.send("https://meet.google.com/duu-uqec-tjv?authuser=0&hs=179")
-			
-		else:
-			await ctx.send("Rn koyi class koni hjdhfjahsdfjkg")
-	elif(day=='Tue'):
-		if(time1==9):
-			await ctx.send("IDBMS's lab")			
-			await ctx.send("teacher tatti h kuch nhi krvayeag so u can sleep")			
-			await ctx.send("https://meet.google.com/lookup/g232xe3juu?authuser=1&hs=179")					
-		elif(time1==1+12):
-			await ctx.send("doubt class h and ik u haven't seen the video so chill kr")
-		else:
-			await ctx.send("mje kr be !!!!!!!!!!!!!!!!")		
-	elif(day=='Wed'):
-		if(time1==9):
-			await ctx.send("https://meet.google.com/tif-qahu-sau?authuser=0")
-		elif(time1==5+12):
-			await ctx.send("AP by varun")			
-			await ctx.send("https://meet.google.com/lookup/htvdf4mslf")			
-		elif(time1==1+12):
-			await ctx.send("COA")			
-			await ctx.send("https://meet.google.com/bnd-gsza-oyf?pli=1&authuser=1")
-		elif(time1==2+12):
-			await ctx.send("IDBMS")
-			await ctx.send("https://meet.google.com/lookup/evx4lzq2xa?authuser=1&hs=179")
-		elif(time1==3+12):		
-			await ctx.send("DCS")
-			await ctx.send("https://meet.google.com/lookup/gcfpz2o5ch?authuser=1&hs=179")
-		elif(time1==4+12):		
-			await ctx.send("M3")
-			await ctx.send("https://meet.google.com/duu-uqec-tjv?authuser=0&hs=179")
-		else:
-			await ctx.send("NO calss hsadfjhsdgah")
-	elif(day=='Thu'):
-		if(time1==10):
-			await ctx.send("AP's lab")			
-			# await ctx.send("teacher tatti h kuch nhi krvayeag so u can sleep")			
-			await ctx.send("https://meet.google.com/lookup/htvdf4mslf")					
-		elif(time1==1+12):
-			await ctx.send("doubt class h and ik u haven't seen the video so chill kr")
-		else:
-			await ctx.send("mje kr be !!!!!!!!!!!!!!!!")		
-	elif(day=="Fri"):
-		if(time1==4+12):
-			await ctx.send("AP by varun")			
-			await ctx.send("https://meet.google.com/lookup/frazjfw4w6?authuser=1&hs=179")			
-		elif(time1==5+12):
-			await ctx.send("COA")			
-			await ctx.send("https://meet.google.com/bnd-gsza-oyf?pli=1&authuser=1")
-		elif(time1==1+12):
-			await ctx.send("IDBMS")
-			await ctx.send("https://meet.google.com/lookup/evx4lzq2xa?authuser=1&hs=179")
-		elif(time1==2+12):		
-			await ctx.send("DCS")
-			await ctx.send("https://meet.google.com/lookup/gcfpz2o5ch?authuser=1&hs=179")
-		elif(time1==3+12):		
-			await ctx.send("M3")
-			await ctx.send("https://meet.google.com/duu-uqec-tjv?authuser=0&hs=179")
-		else:
-			await ctx.send("NO calss hsadfjhsdgah")
-	else:
-		await ctx.send("i duuno jk no class today sjkhsgd")
 
 if __name__ == "__main__":
     client.run(TOKEN)
